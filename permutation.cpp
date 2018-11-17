a1f2837eb0cd0c64f8f2ef0a14814c252b86e3b1
eslam11993377 <36113402+eslam11993377@users.noreply.github.com>
2018-11-17 22:49:11 +0200

    Update permutation.cpp

--- permutation.cpp
+++ permutation.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
-
-void permutation(string s,int i,int n)
+void permutation(string s,int i,int n)///(my word , beginning index of the string , last index of the string)
 {
     int j;
-    if (i == n)
-        cout << s << "\t";
+    if (i == n)///if i choose all characters of the string
+        cout << s << "\t";///print the string
     else
     {
         for (j = i; j < s.length(); j++)
         {
-            char ch =s[j];
-            swap(s[i],s[j]);
-            permutation(s, i + 1, n);
-            swap(s[i],ch);
+            char ch =s[j];///put the first character of the string in ch
+            swap(s[i],s[j]);///ordinary swap to 2 character
+            permutation(s, i + 1, n);///recursion to take the remaining letters
+            swap(s[i],ch);///swap s[i] with the first character of the string that i taken in any loops
         }
     }
 }
 
+
 int main()
 {
-
-
     string s;
     cout << "Enter the string : ";
     cin >> s;
     cout << endl << "The permutations of the given string : " << endl;
-    //p(s);
     permutation(s, 0, s.length()- 1);
     cout << endl;
 }
-
